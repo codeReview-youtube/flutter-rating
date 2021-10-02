1525c99f9c4848bc8e9cd292d89ac625c3a111b2
Mustafa Alroomi <mustafa@Mustafas-Mac-mini.local>
2021-10-03 00:52:21 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/in_app_review_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.in_app_review_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import './rating_service.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final RatingService _ratingService = RatingService();
+
+  @override
+  initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      _ratingService.isSecondTimeOpen().then((secondOpen) {
+        if (secondOpen) {
+          _ratingService.showRating();
+        }
+      });
+    });
+  }
+
+  Future<void> _incrementCounter(context) async {
+    setState(() {
+      _counter++;
+    });
+    /**
+     * In demand rating flow
+     */
+    if (_counter == 5) {
+      _ratingService.showRating();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _incrementCounter(context),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
